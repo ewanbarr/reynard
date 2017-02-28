b0474ceeebe1b22e3cc232f0d3e5be89e4bb65a0
Ewan Barr <ewan.d.barr@googlmail.com>
2017-02-28 15:46:14 +0100

    updated cli to use cmd2 and created generic utils file

--- scripts/reynard_basic_cli.py
+++ scripts/reynard_basic_cli.py
@@ -1,49 +1,57 @@
-#!/usr/bin/env python
-
-"""KATCP command line client example.
-
-   copyright (c) 2008 SKA/KAT. All Rights Reserved.
-   @author Simon Cross <simon.cross@ska.ac.za>
-   @date 2008-10-29
-"""
-
 import logging
 import sys
 import traceback
-from optparse import OptionParser
 import katcp
-import cPickle
-
-from katcp import DeviceClient
+import readline
+from optparse import OptionParser
+from cmd2 import Cmd
+from reynard.utils import StreamClient
 
 logging.basicConfig(level=logging.INFO,
                     stream=sys.stderr,
                     format="%(asctime)s - %(name)s - %(filename)s:"
                     "%(lineno)s - %(levelname)s - %(message)s")
 
-class FbfuseCmdlineClient(DeviceClient):
+log = logging.getLogger("reynard.basic_cli")
 
-    def reply_multicast_beam_mapping(self,msg):
-        """arse"""
-        response,data = msg.arguments
-        mapping = cPickle.loads(data)
-        print mapping
 
-    def unhandled_reply(self,msg):
-        """Deal with unhandled replies"""
-        print "Unhandled reply:",msg
+class KatcpCli(Cmd):
+    """
+    @brief      Basic command line interface to KATCP device
 
-    def unhandled_inform(self,msg):
-        """Deal with unhandled replies"""
-        print "Unhandled inform:",msg
+    @detail     This class provides a command line interface to
+                to any katcp.DeviceClient subclass. Behaviour of the
+                interface is determined by the client object passed
+                at instantiation.
+    """
+    Cmd.shortcuts.update({'?': 'katcp'})
+    def __init__(self,client,*args,**kwargs):
+        """
+        @brief  Instantiate new KatcpCli instance
 
-    def unhandled_request(self,msg):
-        """Deal with unhandled replies"""
-        print "Unhandled request:",msg
+        @params client A DeviceClient instance
+        """
+        self.client = client
+        Cmd.__init__(self,args,kwargs)
 
+    def do_katcp(self, arg, opts=None):
+        """
+        @brief      Send a request message to a katcp server
+
+        @param      arg   The request
+        """
+        request = "?" + "".join(arg)
+        log.info("Request: %s"%request)
+        try:
+            msg = katcp_parser.parse(request)
+            client.ioloop.add_callback(client.send_message, msg)
+        except Exception, e:
+            e_type, e_value, trace = sys.exc_info()
+            reason = "\n".join(traceback.format_exception(
+                e_type, e_value, trace, 20))
+            log.exception(reason)
 
 if __name__ == "__main__":
-
     usage = "usage: %prog [options]"
     parser = OptionParser(usage=usage)
     parser.add_option('-a', '--host', dest='host', type="string", default="", metavar='HOST',
@@ -51,25 +59,16 @@ if __name__ == "__main__":
     parser.add_option('-p', '--port', dest='port', type=int, default=1235, metavar='N',
                       help='attach to server port N (default=1235)')
     (opts, args) = parser.parse_args()
-
     katcp_parser = katcp.MessageParser()
-
-    print "Client connecting to port %s:%d, Ctrl-C to terminate." % (opts.host, opts.port)
-    client = FbfuseCmdlineClient(opts.host, opts.port)
-
+    log.info("Client connecting to port %s:%d, Ctrl-C to terminate." % (opts.host, opts.port))
+    client = StreamClient(opts.host, opts.port)
     client.start()
     try:
-        while True:
-            s = raw_input("> ")
-            try:
-                msg = katcp_parser.parse(s)
-                client.ioloop.add_callback(client.send_message, msg)
-            except Exception, e:
-                e_type, e_value, trace = sys.exc_info()
-                reason = "\n".join(traceback.format_exception(
-                    e_type, e_value, trace, 20
-                ))
-                print reason
+        app = KatcpCli(client)
+        app.prompt = "(katcp CLI %s:%d) " % (opts.host, opts.port)
+        app.cmdloop()
+    except Exception as error:
+        log.exception("Error from CLI")
     finally:
         client.stop()
         client.join()
