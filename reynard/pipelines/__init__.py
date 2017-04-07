4b6020ddcfd623467f1d1f17db9993d958c97ad9
Ewan Barr <ewan.d.barr@googlmail.com>
2017-04-07 13:03:11 +0200

    added support for PafPipeline

--- reynard/pipelines/__init__.py
+++ reynard/pipelines/__init__.py
@@ -1,3 +1,4 @@
 from pipeline import Pipeline, DockerHelper, reynard_pipeline, PIPELINE_REGISTRY, PIPELINE_STATES, PipelineError
 from junk_2_db_2_null import Junk2Db2Null
 from udp_2_db_2_dspsr import Udp2Db2Dspsr
+from paf_2_fil import PafFrbPipeline
--- reynard/pipelines/paf_2_fil.py
+++ reynard/pipelines/paf_2_fil.py
@@ -37,46 +37,27 @@ class PafFrbPipeline(Pipeline):
         self._volumes.append("{}:/output/".format(config["output_path"]))
 
     def _start(self, sensors):
-        header = self._config["dada_header_params"]
         header["ra"] = sensors["ra"]
         header["dec"] = sensors["dec"]
         source_name = sensors["source-name"]
-        try:
-            source_name = source_name.split("_")[0]
-        except BaseException:
-            pass
         header["source_name"] = source_name
         header["obs_id"] = "{0}_{1}".format(
             sensors["scannum"], sensors["subscannum"])
 
-        dada_header_file = tempfile.NamedTemporaryFile(
+        paf_config_file = tempfile.NamedTemporaryFile(
             mode="w",
-            prefix="reynard_dada_header_",
+            prefix="reynard_paf_config_",
             suffix=".txt",
             dir="/tmp/",
             delete=False)
         log.debug(
-            "Writing dada header file to {0}".format(
-                dada_header_file.name))
-        header_string = render_dada_header(header)
-        dada_header_file.write(header_string)
-        log.debug("Header file contains:\n{0}".format(header_string))
-        dada_key_file = tempfile.NamedTemporaryFile(
-            mode="w",
-            prefix="reynard_dada_keyfile_",
-            suffix=".key",
-            dir="/tmp",
-            delete=False)
-        log.debug("Writing dada key file to {0}".format(dada_key_file.name))
-        key_string = make_dada_key_string(self._dada_key)
-        dada_key_file.write(make_dada_key_string(self._dada_key))
-        log.debug("Dada key file contains:\n{0}".format(key_string))
-        dada_header_file.close()
-        dada_key_file.close()
+            "Writing paf config file to {0}".format(
+                paf_config_file.name))
+        for line in self._config["config_file_params"]:
+            paf_config_file.write(line+"\n")
+        paf_config_file.close()
 
-        self._set_watchdog("dspsr", callback=self.stop)
-        self._set_watchdog("udp2db")
-        self._set_watchdog("dbmonitor", persistent=True)
+        self._set_watchdog("paf", persistent=True)
 
         ulimits = [{
             "Name": "memlock",
@@ -84,42 +65,18 @@ class PafFrbPipeline(Pipeline):
             "Soft": -1
         }]
 
-        cmd = "dspsr {args} -N {source_name} {keyfile}".format(
-            args=self._config["dspsr_params"]["args"],
-            source_name=source_name,
-            keyfile=dada_key_file.name)
+        cmd = "pafrb --config {config} -o /output/ -v".format(
+            config=paf_config_file.name)
         log.debug("Running command: {0}".format(cmd))
-        self._docker.run(self._config["dspsr_params"]["image"], cmd,
-                         detach=True, name="dspsr", ipc_mode="host",
-                         volumes=self._volumes, ulimits=ulimits,
+        self._docker.run(self._config["image"], cmd,
+                         detach=True, name="paf",
+                         volumes=self._volumes,
+                         network_mode="host",
+                         ulimits=ulimits,
                          requires_nvidia=True)
 
-        cmd = ("LD_PRELOAD=libvma.so udp2db "
-               "-k {key} {args} -H {headerfile}").format(
-            key=self._dada_key,
-            args=self._config["udp2db_params"]["args"],
-            headerfile=dada_header_file.name)
-        log.debug("Running command: {0}".format(cmd))
-        self._docker.run(
-            self._config["udp2db_params"]["image"],
-            cmd,
-            detach=True,
-            volumes=self._volumes,
-            name="udp2db",
-            ipc_mode="host",
-            network_mode="host",
-            requires_vma=True,
-            ulimits=ulimits)
-
-        cmd = "dada_dbmonitor -k {key} {args}".format(
-            key=self._dada_key,
-            args=self._config["dada_dbmonitor_params"]["args"])
-        log.debug("Running command: {0}".format(cmd))
-        self._docker.run(self._config["dada_dbmonitor_params"]["image"], cmd,
-                         detach=True, name="dbmonitor", ipc_mode="host")
-
     def _stop(self):
-        for name in ["dspsr", "udp2db", "dbmonitor"]:
+        for name in ["paf"]:
             container = self._docker.get(name)
             try:
                 log.debug(
@@ -133,21 +90,18 @@ class PafFrbPipeline(Pipeline):
                     "Removing {name} container".format(
                         name=container.name))
                 container.remove()
-            except BaseException:
+            except Exception:
                 pass
 
     def _deconfigure(self):
-        log.debug("Destroying dada buffer")
-        cmd = "dada_db -d -k {0}".format(self._dada_key)
-        log.debug("Running command: {0}".format(cmd))
-        self._docker.run("psr-capture", cmd, remove=True, ipc_mode="host")
+        pass
 
     def _status(self):
         reply = {}
         reply["state"] = self.state
         if self.state == "running":
             container_info = []
-            for name in ["dspsr", "udp2db", "dbmonitor"]:
+            for name in ["paf"]:
                 container = self._docker.get(name)
                 detail = {
                     "name": container.name,
