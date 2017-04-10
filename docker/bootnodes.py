1ce407d288ac3bc96b43875462c1924f560c454e
Ewan Barr <ewan.d.barr@googlmail.com>
2017-04-10 22:54:55 +0200

    updated bootnodes to not depend on reynard

--- docker/bootnodes.py
+++ docker/bootnodes.py
@@ -1,12 +1,11 @@
+import sys
 import socket
 import json
-import reynard.effelsberg.config as config_manager
 
 IMAGE = "docker.mpifr-bonn.mpg.de:5000/reynard:latest"
 
 def bootnode(hostname,port=5100):
-    #ip = socket.gethostbyname(hostname)
-    ip = "127.0.0.1"
+    ip = socket.gethostbyname(hostname)
     cmd = ("reynard_ubn_server.py --host {ip} "
            "--port {port} --log_level DEBUG").format(
            ip=ip,port=port)
@@ -14,15 +13,34 @@ def bootnode(hostname,port=5100):
               "--name ubn-server --net=host "
               "-v /dev/:/host-dev/ -v /tmp/:/tmp/ "
               "-v /var/run/docker.sock:/var/run/docker.sock "
-              "{image} {cmd}").format(image=IMAGE, cmd=cmd, 
+              "{image} {cmd}").format(image=IMAGE, cmd=cmd,
                                       port=port)
     print ("ssh {0} {1}".format(hostname,docker))
 
-def main(nodeset):
-    nodes = config_manager.get_nodes(nodeset)
+def main(nodes):
     for node in nodes:
-        bootnode(node["host"])
+      bootnode(node)
 
 if __name__ == "__main__":
-    main("effelsberg")
+    from argparse import ArgumentParser
+    usage = "{prog} [options]".format(prog=sys.argv[0])
+    parser = ArgumentParser(usage=usage)
+    parser.add_argument('-n','--nodes', nargs='+',
+        help='Hosts to boot ubn servers on',
+        default=None, required=False)
+    parser.add_argument('-c','--config', nargs=1, type=str,
+        help='Configuration containing nodes to be booted',
+        default=None, required=False)
+    args = parser.parse_args()
+
+    if args.config:
+        with open(args.config) as f:
+            conf = json.loads(f)
+            nodes = [node['host'] for node in conf]
+    elif args.nodes:
+        nodes = args.nodes
+    else:
+        print "Must specify a cconfig file or a list of nodes"
+        sys.exit()
+    main(nodes)
 
