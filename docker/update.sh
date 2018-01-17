670f953b3bed950cbd5b55dd539ccc9014f8bf46
ewanbarr <ewan.d.barr@gmail.com>
2018-01-17 19:00:40 +0100

    fixed template error

--- docker/update.sh
+++ docker/update.sh
@@ -1,3 +1,3 @@
-docker run --name reynard-update reynard bash -c "cd /soft/reynard/ ; git pull ; git checkout monitor_pipeline; python setup.py install"
+docker run --name reynard-update reynard bash -c "cd /soft/reynard/ ; git pull ; git checkout dev; python setup.py install"
 docker commit reynard-update reynard:latest
 docker rm reynard-update
